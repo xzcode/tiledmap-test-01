d79a548fb398f048a78e414a09d09e8b4e428a7b
zai <379551875@qq.com>
2020-09-24 23:04:52 +0800

    xx

---
+++ assets/tiledmap/land_54_3.tsx
@@ -0,0 +1,4 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<tileset version="1.4" tiledversion="1.4.2" name="land_54_3" tilewidth="400" tileheight="200" tilecount="1" columns="1">
+ <image source="land_54_3.png" width="400" height="220"/>
+</tileset>
--- assets/tiledmap/scv.ts
+++ assets/tiledmap/scv.ts
@@ -15,7 +15,7 @@ export default class NewClass extends cc.Component {
     @property(cc.Canvas)
     canvas: cc.Canvas = null;
     @property(cc.SpriteFrame)
-    node1Sprite: cc.SpriteFrame = null;
+    node1SpriteFrame: cc.SpriteFrame = null;
 
 
     // LIFE-CYCLE CALLBACKS:
@@ -29,15 +29,20 @@ export default class NewClass extends cc.Component {
 
         // 创建一个新节点
         let node1 = new cc.Node();
+        node1.width = 400;
+        node1.height = 200;
+        cc.node1 = node1;
         // 然后把该节点的父节点设置为任意的 layer 节点
         node1.parent = this.tiledLayer.node;  
         // 最后添加 TiledTile 组件到该节点上，并返回 TiledTile 对象，就可以对 TiledTile 对象进行一系列操作
         let tiledTile = node1.addComponent(cc.TiledTile);
         tiledTile.x = 141;
         tiledTile.y = 142;
+        
+        tiledTile.enabled = true;
         let node1Sprite = node1.addComponent(cc.Sprite);
-        node1Sprite.spriteFrame = this.node1Sprite;
-        console.log(node1)
+        node1Sprite.spriteFrame = this.node1SpriteFrame;
+        node1Sprite.enabled = true;
     }
 
     start () {
