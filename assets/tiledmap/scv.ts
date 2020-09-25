9e7f2505cf705f6acc2e5aad8610e6590e925778
zzz <379551875@qq.com>
2020-09-25 17:31:53 +0800

    xx

--- assets/tiledmap/scv.ts
+++ assets/tiledmap/scv.ts
@@ -26,23 +26,28 @@ export default class NewClass extends cc.Component {
         cc.tiledLayer = this.tiledLayer;
         cc.camera01 = this.camera;
         cc.xml2 = require('xml2js')
-
-        // 创建一个新节点
-        let node1 = new cc.Node();
-        node1.width = 400;
-        node1.height = 200;
-        cc.node1 = node1;
-        // 然后把该节点的父节点设置为任意的 layer 节点
-        node1.parent = this.tiledLayer.node;  
-        // 最后添加 TiledTile 组件到该节点上，并返回 TiledTile 对象，就可以对 TiledTile 对象进行一系列操作
-        let tiledTile = node1.addComponent(cc.TiledTile);
-        tiledTile.x = 141;
-        tiledTile.y = 142;
+        cc.resources.load("citys", (e,a:cc.JsonAsset) => {
+        const array = a.json.city;
+        for (let i = 0; i < array.length; i++) {
+            const ci = array[i];
+            // 创建一个新节点
+            let node1 = new cc.Node();
+            node1.width = 400;
+            node1.height = 200;
+            cc.node1 = node1;
+            // 然后把该节点的父节点设置为任意的 layer 节点
+            node1.parent = this.tiledLayer.node;  
+            // 最后添加 TiledTile 组件到该节点上，并返回 TiledTile 对象，就可以对 TiledTile 对象进行一系列操作
+            let tiledTile = node1.addComponent(cc.TiledTile);
+            tiledTile.x = ci.coordX;
+            tiledTile.y = ci.coordY;
+            tiledTile.gid=3;
+            tiledTile.enabled = true;
+        }
         
-        tiledTile.enabled = true;
-        let node1Sprite = node1.addComponent(cc.Sprite);
-        node1Sprite.spriteFrame = this.node1SpriteFrame;
-        node1Sprite.enabled = true;
+
+        });
+        
     }
 
     start () {
